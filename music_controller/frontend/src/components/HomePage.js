71e8f281abed0b1c449ab86d4e260cf14643e330
Lewis Halstead <lewishalstead5@gmail.com>
2023-10-01 20:37:17 +0100

    json error :(

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -11,7 +11,8 @@ export default class HomePage extends Component {
     super(props);
     this.state = {
       roomCode: null
-    }
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this)
   }
 
   async componentDidMount() {
@@ -46,6 +47,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode(){
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
@@ -57,7 +64,12 @@ export default class HomePage extends Component {
           />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create" component={CreateRoomPage} />
-          <Route path='/room/:roomCode' component={Room} />
+          <Route 
+            path='/room/:roomCode' 
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />
+            }}
+          />
         </Switch>
       </Router>
     );
